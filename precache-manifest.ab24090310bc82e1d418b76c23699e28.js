1546db23ed038797c0f307b655f0d2d272d08192
Jordi Pascual Selles <50363892+jordimuro@users.noreply.github.com>
2024-09-17 22:40:08 +0200

    Add files via upload

---
+++ precache-manifest.ab24090310bc82e1d418b76c23699e28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e843f616a23a7199e28bf8b7f46344a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8da6c819bfc2cbc59d7",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "e293f3371241522bf62d",
+    "url": "/static/css/main.2e8f7bc3.chunk.css"
+  },
+  {
+    "revision": "c8da6c819bfc2cbc59d7",
+    "url": "/static/js/2.71011054.chunk.js"
+  },
+  {
+    "revision": "9a6c3bcb7f69e70096783bb0f3d49144",
+    "url": "/static/js/2.71011054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e293f3371241522bf62d",
+    "url": "/static/js/main.01d45f5e.chunk.js"
+  },
+  {
+    "revision": "cf40297c82dbd2adf08e",
+    "url": "/static/js/runtime-main.90b7822a.js"
+  }
+]);
